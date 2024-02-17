d3f18a27b2882826081f22d2ca9428162db3b416
Łukasz Brzózko <lukasz9099@gmail.com>
2024-02-17 21:28:42 +0100

    config: update metadata

--- src/index.user.js
+++ src/index.user.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name         CMS Messages/Settings Template Generator
-// @namespace    http://tampermonkey.net/
-// @version      2024-02-15
-// @description  try to take over the world!
-// @author       Łukasz Brzózko
-// @match        https://dev-control-panel-orbico.nd0.pl/*
-// @match        https://uat-control-panel-orbico.nd0.pl/*
-// @match        https://preprod-control-panel-orbico.nd0.pl/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
 (function () {
   "use strict";
 
